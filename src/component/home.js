865adb56f38a280d343ef9a0305708e1fa26ba7e
Kim Sean Pusod <kimsean@MacBook-Pro.local>
2019-08-06 22:27:18 +0800

    added rules for required and legnth

---
+++ src/component/home.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import validate from '../helper'
+
+export default class Home extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      form: {
+        val_1: '',
+        val_2: '',
+        val_3: ''
+      },
+      rules: {
+        FnameRule: {
+          length: 10,
+          required: true
+        },
+        MnameRule: {
+          length: 5,
+          required: false
+        },
+        LnameRule: {
+          length: 5,
+          required: true
+        }
+      }
+    }
+  }
+  render() {
+    return (
+      <div>
+        <div>
+          <input type="text" rule="FnameRule" error-style="error1" formref="form1" k-validate="true" onChange={(event) => { this.setState({form: {...this.state.form, val_1: event.target.value}})  }}/> <br/>
+          <input type="text" rule="MnameRule" error-style="error2" formref="form1" k-validate="true" onChange={(event) => { this.setState({form: {...this.state.form, val_2: event.target.value}})  }}/> <br/>
+          <input type="text" rule="LnameRule" error-style="error3" formref="form1" k-validate="true" onChange={(event) => { this.setState({form: {...this.state.form, val_3: event.target.value}}) }}/> <br/>
+          {/* <select formref="form1" rule="LnameRule" error-style="error3" k-validate="true" >
+            <option value="1">1</option>
+            <option value="2">2</option>
+          </select> */}
+          <button
+          onClick={(event) => {
+            validate("form1",this.state.rules)
+          }}>Submit</button>
+        </div>
+      </div>
+    )
+  }
+}
---
+++ src/helper/index.js
@@ -0,0 +1,68 @@
+//Elements
+  //rule
+  //formref
+  //error-style
+
+let GlobalRules
+var validate = function (formName, rules) {
+  try {
+    GlobalRules = rules
+    let elements = document.querySelectorAll(`[formref='${formName}']`)
+    for (let el = 0; el < elements.length; el++ ) {
+      checkNode(elements[el])
+    }
+  } catch (err) { console.log(err) }
+}
+
+function getRule (ruleName) {
+  for(let key in GlobalRules){
+    if (key === ruleName) {
+      return GlobalRules[key]
+    }
+  }
+}
+
+function checkNode (element) {
+  let ruleName = element.getAttribute('rule')
+  if (element.nodeName === 'INPUT') {
+    validateInputNode(element,ruleName)
+  }
+}
+
+
+
+function returnUnvalidatedElement (element) {
+  let errorStyle = element.getAttribute('error-style')
+  if (errorStyle) {
+    element.classList.add(errorStyle)
+  } else {
+    return false
+  }
+}
+
+function ruleCheckRequired(element) {
+  if(!element.value || element.value.length === 0){
+    return returnUnvalidatedElement(element)
+  }
+}
+
+function ruleCheckLength (element, length) {
+  if (element.value.length > length) {
+    return returnUnvalidatedElement(element)
+  }
+}
+
+function validateInputNode (element, ruleName) {
+  let ruleList = getRule(ruleName)
+  for(let key in ruleList){
+    if (key === 'required') {
+      if (ruleList[key] === true) {
+        ruleCheckRequired(element,ruleList[key])
+      }
+    }
+    if (key === 'length') {
+      ruleCheckLength(element,ruleList[key])
+    }
+  }
+}
+export default validate
