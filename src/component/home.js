0d6ae9f7b69e39983a89cccf10b8294f40759c94
Kim Sean Pusod <kimsean@MacBook-Pro.local>
2019-08-07 23:45:59 +0800

    added validation for group radio

--- src/component/home.js
+++ src/component/home.js
@@ -26,6 +26,9 @@ export default class Home extends React.Component {
         EmailRule: {
           required: true,
           email: true
+        },
+        RadioRule: {
+          required: true
         }
       }
     }
@@ -38,10 +41,21 @@ export default class Home extends React.Component {
           <input type="text" rule="MnameRule" error-style="error2" formref="form1" k-validate="true" onChange={(event) => { this.setState({form: {...this.state.form, val_2: event.target.value}})  }}/> <br/>
           <input type="text" rule="LnameRule" error-style="error3" formref="form1" k-validate="true" onChange={(event) => { this.setState({form: {...this.state.form, val_3: event.target.value}}) }}/> <br/>
           <input type="email" rule="EmailRule" error-style="error3" formref="form1" k-validate="true" onChange={(event) => { this.setState({form: {...this.state.form, val_3: event.target.value}}) }}/> <br/>
-          {/* <select formref="form1" rule="LnameRule" error-style="error3" k-validate="true" >
+          <select type="select" formref="form1" rule="LnameRule" error-style="error3" k-validate="true" >
             <option value="1">1</option>
             <option value="2">2</option>
-          </select> */}
+          </select>
+          <br/>
+          <input formref="form1" rule="RadioRule" k-validate="true" type="radio" name="gender" value="male"/>
+          <input formref="form1" rule="RadioRule" k-validate="true" type="radio" name="gender" value="female"/>
+          <input formref="form1" rule="RadioRule" k-validate="true" type="radio" name="gender" value="other"/>
+
+          <br/>
+          <br/>
+          <input className="testing" k-validate="true" type="radio" name="location" value="male"/>
+          <input k-validate="true" type="radio" name="location" value="female"/>
+          <input k-validate="true" type="radio" name="location" value="other"/>
+          <br/>
           <button
           onClick={(event) => {
             let valid = validate("form1",this.state.rules)
